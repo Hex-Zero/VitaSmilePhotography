44e8f25c7d46a94771663bf892670aa6d0835eb4
Hex-Zero <hex0@live.com>
2019-08-11 10:17:43 +0100

    hotfix instagram hover bug

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react"
-import Navigation from "./components/Navigation"
-import SocialMedia from "./components/SocialMedia"
-import Home from "./components/Home"
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import About from "./components/About"
-import Pricing from "./components/Pricing"
-import Footer from "./components/Footer"
+import React from 'react'
+import Navigation from './components/Navigation'
+import SocialMedia from './components/SocialMedia'
+import Home from './components/Home'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import About from './components/About'
+import Pricing from './components/Pricing'
+import Footer from './components/Footer'
 
 const App = () => {
-  return (
-    <Router>
-      <Navigation />
-      <Route path="/" exact component={Home} />
-      <Route path="/About" component={About} />
-      <Route path="/Pricing" component={Pricing} />
-      <SocialMedia />
-      <Footer />
-    </Router>
-  )
+	return (
+		<Router>
+			<Navigation />
+			<Route path="/" exact component={Home} />
+			<Route path="/About" component={About} />
+			<Route path="/Pricing" component={Pricing} />
+			<SocialMedia />
+			<Footer />
+		</Router>
+	)
 }
 
 export default App
--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,72 +1,72 @@
-import React from "react"
-import styled from "styled-components"
-import { FaFacebookSquare, FaInstagram } from "react-icons/fa"
+import React from 'react'
+import styled from 'styled-components'
+import { FaFacebookSquare, FaInstagram } from 'react-icons/fa'
 
 const SocialMedia = () => {
-  return (
-    <Styled>
-      <a
-        className="facebook"
-        href="https://www.facebook.com/vitasmilephotography"
-        target="_blank"
-        rel="noopener noreferrer">
-        <FaFacebookSquare size={43} />
-      </a>
-      <a
-        className="instagram"
-        href="https://urlgeni.us/instagram/vita.smile"
-        target="_blank"
-        rel="noopener noreferrer">
-        <FaInstagram className="fa-instagram" size={43} />
-      </a>
-      <svg width="0" height="0">
-        <radialGradient id="rg" r="150%" cx="30%" cy="107%">
-          <stop stop-color="#fdf497" offset="0" />
-          <stop stop-color="#fdf497" offset="0.05" />
-          <stop stop-color="#fd5949" offset="0.45" />
-          <stop stop-color="#d6249f" offset="0.6" />
-          <stop stop-color="#285AEB" offset="0.9" />
-        </radialGradient>
-      </svg>
-      <svg width="0" height="0">
-        <radialGradient id="rgi" r="150%" cx="30%" cy="107%">
-          <stop stop-color="#e1d989" offset="0" />
-          <stop stop-color="#e1d989" offset="0.05" />
-          <stop stop-color="#ce483b" offset="0.45" />
-          <stop stop-color="#c42192" offset="0.6" />
-          <stop stop-color="#214dce" offset="0.9" />
-        </radialGradient>
-      </svg>
-    </Styled>
-  )
+	return (
+		<Styled>
+			<a
+				className="facebook"
+				href="https://www.facebook.com/vitasmilephotography"
+				target="_blank"
+				rel="noopener noreferrer">
+				<FaFacebookSquare size={43} />
+			</a>
+			<a
+				className="instagram"
+				href="https://urlgeni.us/instagram/vita.smile"
+				target="_blank"
+				rel="noopener noreferrer">
+				<FaInstagram className="fa-instagram" size={43} />
+			</a>
+			<svg width="0" height="0">
+				<radialGradient id="rg" r="150%" cx="30%" cy="107%">
+					<stop stop-color="#fdf497" offset="0" />
+					<stop stop-color="#fdf497" offset="0.05" />
+					<stop stop-color="#fd5949" offset="0.45" />
+					<stop stop-color="#d6249f" offset="0.6" />
+					<stop stop-color="#285AEB" offset="0.9" />
+				</radialGradient>
+			</svg>
+			<svg width="0" height="0">
+				<radialGradient id="rgi" r="150%" cx="30%" cy="107%">
+					<stop stop-color="#e1d989" offset="0" />
+					<stop stop-color="#e1d989" offset="0.05" />
+					<stop stop-color="#ce483b" offset="0.45" />
+					<stop stop-color="#c42192" offset="0.6" />
+					<stop stop-color="#214dce" offset="0.9" />
+				</radialGradient>
+			</svg>
+		</Styled>
+	)
 }
 
 const Styled = styled.div`
-  .facebook {
-    width: 44px;
-    display: block;
-    position: fixed;
-    right: 1px;
-    top: 503px;
-  }
-  .instagram svg * {
-    fill: url(#rg);
-  }
+	.facebook {
+		width: 44px;
+		display: block;
+		position: fixed;
+		right: 1px;
+		top: 503px;
+	}
+	.instagram svg * {
+		fill: url(#rg);
+	}
 
-  .instagram svg *:hover {
-    fill: url(#rgi);
-  }
+	.instagram:hover svg * {
+		fill: url(#rgi);
+	}
 
-  .instagram {
-    width: 44px;
-    display: block;
-    position: fixed;
-    right: 1px;
-    top: 545px;
-  }
-  .a {
-    padding-right: 10px;
-  }
+	.instagram {
+		width: 44px;
+		display: block;
+		position: fixed;
+		right: 1px;
+		top: 545px;
+	}
+	.a {
+		padding-right: 10px;
+	}
 `
 
 export default SocialMedia
