2ccf89a396fce1724e441ead0f53598aa643f82a
Hex-Zero <hex0@live.com>
2019-08-16 17:41:29 +0100

    Added react-snap for better seo and ssr

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
+import React from 'react'
+import './index.css'
+import App from './App'
+import { hydrate, render } from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+	hydrate(<App />, rootElement)
+} else {
+	render(<App />, rootElement)
+}
